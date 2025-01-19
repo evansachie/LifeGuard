2da252be5448745cac4f86a2e811c76285b7f391
Evans Acheampong <evansachie01@gmail.com>
2025-01-19 19:48:20 +0000

    update: make changes to layout

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -56,7 +56,7 @@ export default function HomeScreen() {
         <MenuItem 
           title="Pollution Tracker"
           icon={require('@/assets/pollution-tracker.svg')}
-          onPress={() => router.push("/(tabs)/pollution-tracker")}
+          onPress={() => router.push("/(screens)/pollution-tracker")}
         />
       </View>
     </ScrollView>
