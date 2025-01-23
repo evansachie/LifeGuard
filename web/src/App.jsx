91985fa5959671f35a64b0e87453fde5fbb31672
Evans Acheampong <evansachie01@gmail.com>
2025-01-23 17:47:49 +0000

    update: add PollutionTracker

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -8,6 +8,7 @@ import Sidebar from './components/Sidebar/Sidebar';
 import Dashboard from './pages/Dashboard/Dashboard'
 import PrivateMemos from './pages/PivateMemos/PrivateMemos';
 import HealthReport from "./pages/HealthReport/HealthReport";
+import PollutionTracker from './pages/PollutionTracker/PollutionTracker';
 import Profile from "./pages/Profile/Profile";
 import Settings from "./pages/Settings/Settings";
 import Help from './pages/Help/Help';
@@ -42,9 +43,11 @@ function App() {
                                     <Route path="/dashboard" element={<Dashboard isDarkMode={isDarkMode} />} />
                                     <Route path="/sticky-notes" element={<PrivateMemos isDarkMode={isDarkMode} />} />
                                     <Route path="/health-report" element={<HealthReport isDarkMode={isDarkMode} />} />
+                                    <Route path="/pollution-tracker" element={<PollutionTracker isDarkMode={isDarkMode} />} />
 
                                     <Route path="/profile" element={<Profile isDarkMode={isDarkMode} />} />
                                     <Route path="/settings" element={<Settings isDarkMode={isDarkMode} />} />
+                                    
                                     <Route path="/help" element={<Help isDarkMode={isDarkMode} />} />
                                     <Route path="/terms-of-use" element={<TermsOfUse isDarkMode={isDarkMode} />} />
                                     <Route path="/privacy-policy" element={<PrivacyPolicy isDarkMode={isDarkMode} />} />
