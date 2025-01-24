0fecf87ed0adb07579f94190a96a7555fdf8b39c
Evans Acheampong <evansachie01@gmail.com>
2025-01-24 15:30:18 +0000

    initial commit for node server

---
+++ node-server/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const cors = require('cors');
+const memoRoutes = require('./Routes/memoRoutes');
+const bmrCalculatorRoutes = require('./Routes/bmrCalculatorRoutes')
+const settingsRoutes = require('./Routes/bmrCalculatorRoutes');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+app.use((req, res, next) => {
+    res.on('finish', () => {
+        console.log(`${req.method} ${req.originalUrl} ${res.statusCode}`);
+    });
+    next();
+});
+
+// Middleware to enable CORS
+app.use(cors({
+    origin: ['*'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
+app.use(express.json());
+
+// PostgreSQL pool using the connection string
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
+
+// Test db connection
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+    } else {
+        console.log('Connected to database successfully!');
+        release();
+    }
+});
+
+
+app.use('/api/memos', memoRoutes(pool));
+app.use('/api/calories', bmrCalculatorRoutes(pool));
+app.use('/api/settings', settingsRoutes(pool));
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express server!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
