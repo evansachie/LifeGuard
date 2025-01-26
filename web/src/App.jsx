a810ae5b05a452de60ec4f158ced76fa7fd757d4
Evans Acheampong <evansachie01@gmail.com>
2025-01-26 15:54:20 +0000

    update: make changes to mock data

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -66,6 +66,7 @@ function App() {
                                     <Route path="/terms-of-use" element={<TermsOfUse isDarkMode={isDarkMode} />} />
                                     <Route path="/privacy-policy" element={<PrivacyPolicy isDarkMode={isDarkMode} />} />
                                     <Route path="/analytics" element={<Analytics isDarkMode={isDarkMode} />} />
+                                    
                                     <Route path="*" element={<NotFound isDarkMode={isDarkMode} />} />
                                 </Routes>
                             </div>
