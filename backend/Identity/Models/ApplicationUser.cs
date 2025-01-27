2a887e49818332d09a38710ef40bc15b9fa4c498
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-01-27 15:40:41 +0000

    feat(backend) : Initialize project files

---
+++ backend/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public byte[] SecretKey { get; private set; }
+        public ApplicationUser()
+        {
+            SecretKey = GenerateSecretKey(32);
+        }
+
+        private byte[] GenerateSecretKey(int keySize)
+        {
+            var key = new byte[keySize];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(key);
+            }
+            return key;
+        }
+    }
+}
