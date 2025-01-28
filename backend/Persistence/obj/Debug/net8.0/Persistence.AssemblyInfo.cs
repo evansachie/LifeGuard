2466b2fedea889ff6ac1e4390164096bbd8f4933
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-01-28 14:49:25 +0000

    feat(backend) :  Initialize project file.

--- backend/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ backend/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd33f96b6ae7cc1318ffa22b317665ff310dff34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399640205aedc494a97489f89918894441ac9c10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
