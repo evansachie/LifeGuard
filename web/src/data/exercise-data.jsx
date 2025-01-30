2e91895acfc0319a00c0c86ea1ce779e2f7c19c9
Evans Acheampong <evansachie01@gmail.com>
2025-01-30 18:06:14 +0000

    feat: add data for breathing sound and exercise

---
+++ web/src/data/exercise-data.jsx
@@ -0,0 +1,173 @@
+export const workoutData = {
+  beginner: {
+    warmup: [
+      {
+        id: 'w1',
+        title: 'Dynamic Stretching',
+        description: 'Full body dynamic stretches to prepare for workout',
+        duration: '5 mins',
+        videoUrl: 'https://www.youtube.com/embed/uW3-Ue07H0M',
+        calories: 25,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    cardio: [
+      {
+        id: 'c1',
+        title: 'Light Jogging',
+        description: 'Easy-paced jogging to build endurance',
+        duration: '10 mins',
+        videoUrl: 'https://www.youtube.com/embed/3XbfW90grUk',
+        calories: 100,
+        targetMuscles: ['Legs', 'Core']
+      }
+    ],
+    strength: [
+      {
+        id: 's1',
+        title: 'Basic Strength Training',
+        description: 'Beginner-friendly strength training routine',
+        duration: '17 mins',
+        videoUrl: 'https://www.youtube.com/embed/WIHy-ZnSndA',
+        calories: 150,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    hiit: [
+      {
+        id: 'h1',
+        title: 'Basic HIIT Circuit',
+        description: 'High-intensity interval training for beginners',
+        duration: '20 mins',
+        videoUrl: 'https://www.youtube.com/embed/M0uO8X3_tEA',
+        calories: 200,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    cooldown: [
+      {
+        id: 'cd1',
+        title: 'Cool Down Stretches',
+        description: 'Essential post-workout stretching routine',
+        duration: '5 mins',
+        videoUrl: 'https://www.youtube.com/embed/3w1szPuqY8I',
+        calories: 20,
+        targetMuscles: ['Full Body']
+      }
+    ]
+  },
+  intermediate: {
+    warmup: [
+      {
+        id: 'w1',
+        title: 'Intermediate Dynamic Stretching',
+        description: 'Full body dynamic stretches to prepare for workout',
+        duration: '17 mins',
+        videoUrl: 'https://www.youtube.com/embed/h1AxL1Qp9eA',
+        calories: 50,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    cardio: [
+      {
+        id: 'c1',
+        title: 'Intermediate Light Jogging',
+        description: 'medium-paced jogging to build endurance',
+        duration: '30 mins',
+        videoUrl: 'https://www.youtube.com/embed/c1mBu4tK90k',
+        calories: 120,
+        targetMuscles: ['Legs', 'Core']
+      }
+    ],
+    strength: [
+      {
+        id: 's1',
+        title: 'Intermediate Strength Training',
+        description: 'Beginner-friendly strength training routine',
+        duration: '17 mins',
+        videoUrl: 'https://www.youtube.com/embed/uM9iFSHUIgU',
+        calories: 150,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    hiit: [
+      {
+        id: 'h1',
+        title: 'Intermediate HIIT Circuit',
+        description: 'High-intensity interval training for beginners',
+        duration: '30 mins',
+        videoUrl: 'https://www.youtube.com/embed/uearF2Iorng',
+        calories: 250,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    cooldown: [
+      {
+        id: 'cd1',
+        title: 'Intermediate Cool Down Stretches',
+        description: 'Essential post-workout stretching routine',
+        duration: '30 mins',
+        videoUrl: 'https://www.youtube.com/embed/3w1szPuqY8I',
+        calories: 20,
+        targetMuscles: ['Full Body']
+      }
+    ]
+  },
+  advanced: {
+    warmup: [
+      {
+        id: 'w1',
+        title: 'Intermediate Dynamic Stretching',
+        description: 'Full body dynamic stretches to prepare for workout',
+        duration: '5 mins',
+        videoUrl: 'https://www.youtube.com/embed/uW3-Ue07H0M',
+        calories: 25,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    cardio: [
+      {
+        id: 'c1',
+        title: 'Light Jogging',
+        description: 'Easy-paced jogging to build endurance',
+        duration: '10 mins',
+        videoUrl: 'https://www.youtube.com/embed/3XbfW90grUk',
+        calories: 100,
+        targetMuscles: ['Legs', 'Core']
+      }
+    ],
+    strength: [
+      {
+        id: 's1',
+        title: 'Basic Strength Training',
+        description: 'Beginner-friendly strength training routine',
+        duration: '17 mins',
+        videoUrl: 'https://www.youtube.com/embed/WIHy-ZnSndA',
+        calories: 150,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    hiit: [
+      {
+        id: 'h1',
+        title: 'Basic HIIT Circuit',
+        description: 'High-intensity interval training for beginners',
+        duration: '20 mins',
+        videoUrl: 'https://www.youtube.com/embed/M0uO8X3_tEA',
+        calories: 200,
+        targetMuscles: ['Full Body']
+      }
+    ],
+    cooldown: [
+      {
+        id: 'cd1',
+        title: 'Cool Down Stretches',
+        description: 'Essential post-workout stretching routine',
+        duration: '5 mins',
+        videoUrl: 'https://www.youtube.com/embed/3w1szPuqY8I',
+        calories: 20,
+        targetMuscles: ['Full Body']
+      }
+    ]
+  }
+};
