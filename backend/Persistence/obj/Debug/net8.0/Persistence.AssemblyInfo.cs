192a15ae507b57088d94a89236350395a76ce51f
Evans Acheampong <evansachie01@gmail.com>
2025-02-03 10:41:43 +0000

    refactor: update project files and resolve build issues

--- backend/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ backend/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d41b1d55136119a3f55fa8f41a3d8aaaf5404d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ca8dcc6e7066f6addf6b930fce9cef359c1a92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- mobile/android/app/src/main/kotlin/com/example/mobilef/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/example/mobilef/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mobilef
+package com.example.mobile
 
 import io.flutter.embedding.android.FlutterActivity
 
