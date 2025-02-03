2de4c5613cc5f68a2cf77970d71f569de6b9f9de
Evans Acheampong <evansachie01@gmail.com>
2025-02-03 09:58:54 +0000

    update: initialize flutter for mobile app

--- backend/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ backend/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9eddf9924e908d7ff2aa925e11dd4bf4cb102e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d41b1d55136119a3f55fa8f41a3d8aaaf5404d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ mobile/android/app/src/main/kotlin/com/example/mobilef/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobilef
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
