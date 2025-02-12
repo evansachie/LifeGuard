c3d4066363950c4fedcc2da3fd780f2fbfd50bce
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-02-12 21:17:29 +0000

    feat(backend) : Add CompleteProfile Command

---
+++ backend/Identity/Features/CompleteProfile/CompleteProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.Models.ApiResult;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Features.CompleteProfile
+{
+    public class CompleteProfileCommand : IRequest<Result>
+    {
+        public string Email { get; set; }
+        public string? Gender { get; set; }
+        public int? Weight { get; set; }
+        public int? Height { get; set; }
+    }
+}
