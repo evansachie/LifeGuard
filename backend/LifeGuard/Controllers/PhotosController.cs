a66ce60150c0c1056af5011193adc075b9d539ec
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-02-24 17:45:31 +0000

    feat(backend) : Add PhotosController

---
+++ backend/LifeGuard/Controllers/PhotosController.cs
@@ -0,0 +1,47 @@
+﻿using Application.Contracts.Photos;
+
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeGuard.Controllers
+{
+    public class PhotosController: Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IUserPhotoService _userPhotoService;
+        
+        public PhotosController(IMediator mediator, IUserPhotoService userPhotoService)
+        {
+            _mediator = mediator;
+            _userPhotoService = userPhotoService;
+        }
+
+        [HttpPost("{id}/photo")]
+        public async Task<IActionResult> AddPhoto(string id, IFormFile file)
+        {
+            try
+            {
+                var result = await _userPhotoService.AddUserPhotoAsync(id, file);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            { 
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}/photo")]
+        public async Task<IActionResult> DeletePhoto(string id)
+        {
+            try
+            {
+                await _userPhotoService.DeleteUserPhotoAsync(id);
+                return Ok("Photo deleted");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
