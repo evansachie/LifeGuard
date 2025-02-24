b76beaab03ab52813f611e740ebf6a0956df9646
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-02-24 17:42:51 +0000

    feat(backend) : Add Identity Migrations

--- backend/Identity/Migrations/20250224171803_First Migration.cs
+++ backend/Identity/Migrations/20250224171803_First Migration.cs
@@ -9,7 +9,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Identity.Migrations
 {
     /// <inheritdoc />
-    public partial class First_Migration : Migration
+    public partial class FirstMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -38,7 +38,10 @@ namespace Identity.Migrations
                     Gender = table.Column<string>(type: "text", nullable: true),
                     Weight = table.Column<int>(type: "integer", nullable: true),
                     Height = table.Column<int>(type: "integer", nullable: true),
+                    Bio = table.Column<string>(type: "text", nullable: true),
                     SecretKey = table.Column<byte[]>(type: "bytea", nullable: false),
+                    PhotoUrl = table.Column<string>(type: "text", nullable: true),
+                    PhotoPublicId = table.Column<string>(type: "text", nullable: true),
                     UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
@@ -176,11 +179,11 @@ namespace Identity.Migrations
 
             migrationBuilder.InsertData(
                 table: "AspNetUsers",
-                columns: new[] { "Id", "AccessFailedCount", "Age", "ConcurrencyStamp", "Email", "EmailConfirmed", "Gender", "Height", "LockoutEnabled", "LockoutEnd", "Name", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecretKey", "SecurityStamp", "TwoFactorEnabled", "UserName", "Weight" },
+                columns: new[] { "Id", "AccessFailedCount", "Age", "Bio", "ConcurrencyStamp", "Email", "EmailConfirmed", "Gender", "Height", "LockoutEnabled", "LockoutEnd", "Name", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "PhotoPublicId", "PhotoUrl", "SecretKey", "SecurityStamp", "TwoFactorEnabled", "UserName", "Weight" },
                 values: new object[,]
                 {
-                    { "7a223968-23b4-4652-z7b7-8574d048cdb9", 0, null, "0f30b90b-7d47-4d13-b643-ecf0235f986d", "madugyamfi76@gmail.com", true, null, null, false, null, "Michael Adu-Gyamfi", "madugyamfi76@gmail.com", "madugyamfi76@gmail.com", "AQAAAAIAAYagAAAAECxtXvrv9Ec2Q4iWMTeuQqqGHyYc2shsme8sZFSR8IUj2WOXLeYY/0h7nSegXOhHiA==", null, false, new byte[] { 45, 92, 71, 240, 231, 221, 96, 168, 191, 225, 12, 55, 59, 171, 63, 51, 103, 106, 209, 110, 114, 102, 111, 218, 169, 122, 47, 109, 174, 124, 121, 53 }, "fb1a6efc-cf7c-4543-94f8-df462ec503db", false, "madugyamfi76@gmail.com", null },
-                    { "88k76378-681a-4044-97ed-4ab612217206", 0, null, "d5fe7017-ac7e-445c-9424-eecc0c6a8506", "smartshuttle4@gmail.com", true, null, null, false, null, "System Admin", "SMARTSHUTTLE4@GMAIL.COM", "ADMIN@LOCALHOST.COM", "AQAAAAIAAYagAAAAEJIKtO++vhqxLnzc1lfMGlaVVRvRsSSuyGEcutRH1rA4wkSMkm+09wb9nLm6kUcHEA==", null, false, new byte[] { 84, 158, 170, 176, 218, 55, 64, 60, 124, 70, 117, 210, 187, 30, 176, 240, 233, 220, 23, 10, 226, 191, 67, 88, 235, 11, 98, 26, 146, 216, 192, 150 }, "8fcd4258-dc53-4e72-b6c7-cd3c26525977", false, "admin@localhost.com", null }
+                    { "7a223968-23b4-4652-z7b7-8574d048cdb9", 0, null, null, "0ca96550-113f-4946-b0c1-b0998632ff36", "madugyamfi76@gmail.com", true, null, null, false, null, "Michael Adu-Gyamfi", "madugyamfi76@gmail.com", "madugyamfi76@gmail.com", "AQAAAAIAAYagAAAAENnr/y+OXIRwhLBI4WUiJ8HW95QznZbajaocDT+YP+vSfz0/BZkVUVwOeg5jXcBHdw==", null, false, null, null, new byte[] { 183, 31, 8, 255, 134, 185, 32, 173, 204, 200, 165, 26, 13, 91, 179, 159, 20, 98, 131, 8, 48, 232, 122, 188, 21, 245, 142, 61, 164, 246, 73, 75 }, "198c6662-ce4a-427d-a1f4-6564e623a8cf", false, "madugyamfi76@gmail.com", null },
+                    { "88k76378-681a-4044-97ed-4ab612217206", 0, null, null, "32cd47f8-3e24-4e55-8a7f-c2def450e98f", "smartshuttle4@gmail.com", true, null, null, false, null, "System Admin", "SMARTSHUTTLE4@GMAIL.COM", "ADMIN@LOCALHOST.COM", "AQAAAAIAAYagAAAAEC+EOz1EWIcwMqC1A3IqVsi2VjtWXCAptODjy9lg1uRAePdO1amT3AFs7Zry8aTEvg==", null, false, null, null, new byte[] { 24, 73, 59, 51, 78, 168, 168, 15, 1, 176, 120, 129, 115, 55, 7, 107, 239, 203, 21, 69, 234, 77, 180, 42, 166, 182, 1, 162, 32, 42, 20, 122 }, "063c348e-6400-4359-940b-324efd5bf88c", false, "admin@localhost.com", null }
                 });
 
             migrationBuilder.InsertData(
