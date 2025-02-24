0756bf69846482334a45350bc5a8fbd05d9f3a2d
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-02-24 17:40:50 +0000

    feat(backend) : Add IUserPhotoService

---
+++ backend/Application/Contracts/Photos/IUserPhotoService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Models.Photos;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Photos
+{
+    public interface IUserPhotoService
+    {
+        Task<PhotoUploadResult> AddUserPhotoAsync(string userId, IFormFile file);
+        Task DeleteUserPhotoAsync(string userId);
+    }
+}
