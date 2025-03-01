17e62747d36f1e1c47c2ee5b34a47f0850cf377f
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-03-01 10:39:45 +0000

    fix(backend) : Fix Age returning null

---
+++ backend/Identity/Features/Profile/CompleteProfile/CompleteProfileCommand.cs
@@ -0,0 +1,22 @@
+﻿using Application.Models.ApiResult;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Features.Profile.CompleteProfile
+{
+    public class CompleteProfileCommand : IRequest<Result>
+    {
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ backend/Identity/Features/Profile/CompleteProfile/CompleteProfileCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using Application.Models.ApiResult;
+using Identity.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Identity.Features.Profile.CompleteProfile
+{
+    public class CompleteProfileCommandHandler : IRequestHandler<CompleteProfileCommand, Result>
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CompleteProfileCommandHandler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+
+        }
+
+        public async Task<Result> Handle(CompleteProfileCommand request, CancellationToken cancellationtoken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+            {
+                return new Result(false, ResultStatusCode.BadRequest, "User not Found");
+
+
+            }
+
+            user.Age = request.Age;
+            user.Gender = request.Gender;
+            user.Height = request.Height;
+            user.Weight = request.Weight;
+            user.PhoneNumber = request.PhoneNumber;
+            user.Bio = request.Bio;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                var errors = string.Join("; ", result.Errors.Select(e => e.Description));
+                return new Result(false, ResultStatusCode.BadRequest, errors);
+            }
+
+            return new Result(true, ResultStatusCode.Success, "Profile successfully Updated");
+        }
+    }
+}
