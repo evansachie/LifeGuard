3b5b482c1a402eb238552cd0cfdb92cd2f50811b
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-03-01 10:40:14 +0000

    fix(backend) : fix age returning null

---
+++ backend/Identity/Features/Profile/GetProfile/GetProfileQuery.cs
@@ -0,0 +1,17 @@
+﻿using Application.Models.ApiResult;
+using Application.Models.Photos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Features.Profile.GetProfile
+{
+    public class GetProfileQuery : IRequest<Result<GetProfileDto>>
+    {
+        public string Id { get; set; }
+
+    }
+}
