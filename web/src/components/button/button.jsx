d5d315e1e848f01b4c67d7f051985c97ee158670
Evans Acheampong <evansachie01@gmail.com>
2025-03-04 11:28:26 +0000

    update: add tailwind css color codes

--- web/src/components/button/button.jsx
+++ web/src/components/button/button.jsx
@@ -5,7 +5,7 @@ export default function Button({ text, isLoading, type = "submit", onClick }) {
             onClick={onClick}
             disabled={isLoading}
             className={`w-full py-3 text-white font-semibold rounded-[10px] transition duration-300 flex items-center justify-center
-                ${isLoading ? "bg-[#6c94e0] cursor-not-allowed" : "bg-custom-blue hover:bg-custom-blue-hover"}
+                ${isLoading ? "bg-loading cursor-not-allowed" : "bg-custom-blue hover:bg-custom-blue-hover"}
             `}
         >
             {isLoading ? (
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
     extend: {
       colors: {
         'custom-blue': '#407CE2',
-        'custom-blue-hover': '#2d5bb9'
+        'custom-blue-hover': '#2d5bb9',
+        'loading': '#6c94e0'
       }
     },
   },
