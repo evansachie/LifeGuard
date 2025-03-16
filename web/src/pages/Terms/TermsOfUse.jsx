323bde3996357ecd59584ebf5caedc17cb18fc04
Evans Acheampong <evansachie01@gmail.com>
2025-03-16 16:19:32 +0000

    refactor: modularize WellnessHub

--- web/src/pages/Terms/TermsOfUse.jsx
+++ web/src/pages/Terms/TermsOfUse.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { ArrowLeft } from 'lucide-react';
 import { BackButton } from '../../components/Buttons/backButton';
 
 const TermsOfUse = ({ isDarkMode }) => {
