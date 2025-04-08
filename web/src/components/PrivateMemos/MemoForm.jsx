dd9b5cb95e73de8bd888c2a126874238ecd86e4a
Evans Acheampong <evansachie01@gmail.com>
2025-04-08 14:50:16 +0000

    feat: integrate TinyMCE for markdown functionality

--- web/src/components/PrivateMemos/MemoForm.jsx
+++ web/src/components/PrivateMemos/MemoForm.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import Spinner from '../Spinner/Spinner';
 
 const MemoForm = ({ 
   memo, 
-  setMemo, 
+  setMemo,
+  isDarkMode,
   handleSave, 
   handleCancel, 
   isEditing = false, 
-  saving = false
+  saving = false,
 }) => {
+
+  const handleEditorChange = (content, editor) => {
+    setMemo(content);
+  };
+
   return (
     <div className="memo-input-container">
-      <textarea
-        className={isEditing ? "edit-memo-input" : "memo-input"}
+      <Editor
+        apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
         value={memo}
-        onChange={(e) => setMemo(e.target.value)}
-        placeholder="Write your note here..."
-        autoFocus
+        onEditorChange={handleEditorChange}
+        init={{
+          height: isEditing ? 250 : 200,
+          menubar: false,
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'help', 'wordcount', 'codesample', 'quickbars'
+          ],
+          toolbar: 'undo redo | blocks | ' +
+          'bold italic forecolor | alignleft aligncenter ' +
+          'alignright alignjustify | bullist numlist outdent indent | ' +
+          'removeformat | codesample | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+          skin: isDarkMode ? 'oxide-dark' : 'oxide',
+          content_css: isDarkMode ? 'dark' : 'default',
+          quickbars_selection_toolbar: 'bold italic | quicklink blockquote | codesample',
+          quickbars_insert_toolbar: false,
+          branding: false,
+          promotion: false,
+          statusbar: false,
+
+          auto_focus: !isEditing,
+        }}
       />
-      <div className="memo-input-actions">
-        <button 
-          className="action-button cancel-button" 
+      <div className="memo-input-actions py-4">
+        <button
+          className="action-button cancel-button"
           onClick={handleCancel}
         >
           Cancel
