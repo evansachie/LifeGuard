dd451e22c3ed4562000368287dfbe12e92696bbd
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-04-24 23:37:44 +0000

    fix(backend) : fix syntax error in headers.

--- backend/Identity/Configurations/UserConfiguration.cs
+++ backend/Identity/Configurations/UserConfiguration.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-d dusing DotNetEnv;
+using DotNetEnv;
 
 namespace Identity.Configurations
 {
