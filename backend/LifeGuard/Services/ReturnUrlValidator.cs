85f3fd08b4bd597450b94e4f0ff90f15b77f8c3c
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-15 22:14:19 +0000

    feat(backend) : Add ReturnUrlValidator.cs

---
+++ backend/LifeGuard/Services/ReturnUrlValidator.cs
@@ -0,0 +1,34 @@
+﻿namespace LifeGuard.Services
+{
+    public interface IReturnUrlValidator
+    {
+        bool ValidUrl(string url);
+        string DefaultUrl { get; }
+    }
+
+    public class ReturnUrlValidator : IReturnUrlValidator
+    {
+        public string DefaultUrl { get; }
+        
+        public ReturnUrlValidator(IConfiguration config)
+        {
+            DefaultUrl = config["FRONTEND"];
+
+        }
+        private static readonly string[] _allowedHosts = new[]
+        {
+            "https://lifeguard-vq69.onrender.com",
+            "https://lifeguard-vert.vercel.app"
+        };
+
+        public bool ValidUrl(string url)
+        {
+            if (!Uri.TryCreate(url,UriKind.Absolute,out var uri))
+                return false;
+            return _allowedHosts.Contains(uri.Host);
+        }
+
+
+
+    }
+}
