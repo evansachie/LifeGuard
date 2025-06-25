bec6cb7a83c39703a7e9f2b087876a31eac1e51a
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-25 20:19:55 +0000

    feat(rag) : Add GradeDocuments node

---
+++ rag/app/core/GradeDocuments.py
@@ -0,0 +1,40 @@
+from typing import Literal
+from langgraph.graph import MessagesState
+from langchain.chat_models import init_chat_model
+from app.core.Models.GradeDocuments import GradeDocuments
+
+
+
+GRADE_PROMPT = (
+    "You are a grader assessing relevance of a retrieved document to a user question. \n "
+    "Here is the retrieved document: \n\n {context} \n\n"
+    "Here is the user question: {question} \n"
+    "If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n"
+    "Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."
+)
+
+
+
+grader_model = init_chat_model("openai:gpt-3.5-turbo-0125", temperature=0)
+
+
+def grade_documents(
+    state: MessagesState,
+) -> Literal["generate_answer", "rewrite_question"]:
+    """Determine whether the retrieved documents are relevant to the question."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+
+    prompt = GRADE_PROMPT.format(question=question, context=context)
+    response = (
+        grader_model
+        .with_structured_output(GradeDocuments).invoke(
+            [{"role": "user", "content": prompt}]
+        )
+    )
+    score = response.binary_score
+
+    if score == "yes":
+        return "generate_answer"
+    else:
+        return "rewrite_question"
