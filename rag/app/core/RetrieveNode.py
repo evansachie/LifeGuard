a3e4191555df564be63dcac93374dac66cc22545
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-26 01:02:02 +0000

    Change return object of RetrieveNode

--- rag/app/core/RetrieveNode.py
+++ rag/app/core/RetrieveNode.py
@@ -3,20 +3,26 @@ from app.core.GetUserRetrieverTool import get_user_retriever_tool
 def retrieve_node(state):
     user_id = state.get("user_id")
     retriever_tool = get_user_retriever_tool(user_id)
-    print("retriever_tool", retriever_tool)
     last_msg = state["messages"][-1] if state["messages"] else {}
     query = ""
+    tool_call_id = None
 
     
     if hasattr(last_msg, "tool_calls") and last_msg.tool_calls:
         query = last_msg.tool_calls[0]["args"]["query"]
+        tool_call_id = last_msg.tool_calls[0]["id"]
     elif hasattr(last_msg, "content"):
         query = last_msg.content
-    print(f"Query received: {query}")
+
     tool_result = retriever_tool.invoke(query)
-    print(f"Docs found: {tool_result}")
+
+    new_message = {
+        "role": "tool",
+        "content": tool_result,
+        "tool_call_id": tool_call_id  
+    }
 
     return {
-        **state,  # preserve all previous keys
-        "retrieved_chunks": tool_result,  
+        **state,
+        "messages": state["messages"] + [new_message],
     }
