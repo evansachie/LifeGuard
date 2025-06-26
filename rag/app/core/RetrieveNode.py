4fa0e8624236bbeebad564fb4d8829a51816dfcf
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-26 15:21:59 +0000

    Fix issue with tool_id

--- rag/app/core/RetrieveNode.py
+++ rag/app/core/RetrieveNode.py
@@ -1,28 +1,25 @@
 from app.core.GetUserRetrieverTool import get_user_retriever_tool
+from langchain_core.messages import ToolMessage
 
 def retrieve_node(state):
     user_id = state.get("user_id")
     retriever_tool = get_user_retriever_tool(user_id)
     last_msg = state["messages"][-1] if state["messages"] else {}
-    query = ""
-    tool_call_id = None
 
-    
     if hasattr(last_msg, "tool_calls") and last_msg.tool_calls:
         query = last_msg.tool_calls[0]["args"]["query"]
         tool_call_id = last_msg.tool_calls[0]["id"]
-    elif hasattr(last_msg, "content"):
-        query = last_msg.content
 
-    tool_result = retriever_tool.invoke(query)
+        tool_result = retriever_tool.invoke(query)
 
-    new_message = {
-        "role": "tool",
-        "content": tool_result,
-        "tool_call_id": tool_call_id  
-    }
 
-    return {
-        **state,
-        "messages": state["messages"] + [new_message],
-    }
+        
+        new_message = ToolMessage(content=tool_result, tool_call_id=tool_call_id)
+
+        return {
+            **state,
+            "messages": state["messages"] + [new_message],
+        }
+
+    else:
+        return state
