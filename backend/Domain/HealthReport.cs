a015e9a7b13cdbeb841e2ed75b1c3347f02ad2ea
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-07-03 23:50:30 +0000

    feat(backend) : Add HealthReport class for health metrics tracking

---
+++ backend/Domain/HealthReport.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class HealthReport : BaseEntity
+    {
+        public string DeviceId { get; set; }
+        public DateTime ReportDate { get; set; }
+
+        public string ReportPeriod { get; set; }
+        public int TotalSteps { get; set; }
+        public double AvgAmbientTemp { get; set; }
+        public double AvgHumidity { get; set; }
+        public int AvgAirQualityIndex { get; set; }
+        public double AvgDailySteps { get; set; }
+        public string Status { get; set; }
+
+        public int DataPointCount { get; set; }
+    }
+}
