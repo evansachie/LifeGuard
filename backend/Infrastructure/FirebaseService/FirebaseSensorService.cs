3520ef8861257e74bdda6f5b5cd72d905fc73e3d
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-07-04 00:23:23 +0000

    feat(backend) : Add method for getting data from firebase

---
+++ backend/Infrastructure/FirebaseService/FirebaseSensorService.cs
@@ -0,0 +1,46 @@
+﻿using Domain;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.FirebaseService
+{
+    public class FirebaseSensorService
+    {
+        private readonly FirebaseClient _client;
+
+        public FirebaseSensorService(FirebaseClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<SensorReading>> GetReadingsForDevice(string deviceId)
+        {
+            var readings = await _client
+                    .Child("devices")
+                    .Child(deviceId)
+                    .Child("sensorData")
+                    .OnceAsync<SensorReading>();
+
+
+            return readings.Select(r => r.Object).ToList();
+        }
+
+
+        public async Task<List<SensorReading>> GetReadingsFromDevice30DaysAsync(string deviceId, long startTimestamp, long endTimestamp)
+        {
+            var readings = await _client
+            .Child("devices").Child(deviceId).Child("sensorData")
+            .OrderByKey()
+            .StartAt(startTimestamp.ToString())
+            .EndAt(endTimestamp.ToString())
+            .OnceAsync<SensorReading>();
+
+            return readings.Select(r => r.Object).ToList();
+        }
+    }
+}
