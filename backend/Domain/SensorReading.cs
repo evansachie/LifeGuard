5e4f7908d08b9ed4d8057aa9c3957625f5d5b24d
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-07-04 00:22:48 +0000

    feat(backend) : Add SensorReading.cs

---
+++ backend/Domain/SensorReading.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+using Domain.GroupedSensorData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SensorReading : BaseEntity
+    {
+        public EnvironmentalData environmental { get; set; }
+        public AirQuality  airQuality { get; set; }
+        public MotionData motionData { get; set; }
+        public long timestamp { get; set; }
+        
+
+    }
+}
