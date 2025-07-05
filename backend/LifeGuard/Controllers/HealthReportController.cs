10a0c4f230c773e1ebaab245b1273cba7aa662bd
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-07-05 19:20:21 +0000

    feat(backend) : Add HealthReportController.cs

---
+++ backend/LifeGuard/Controllers/HealthReportController.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Domain.Contracts.Firebase;
+using Domain.Interfaces.HealthReport;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeGuard.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthReportController :ControllerBase
+    {
+        private readonly IHealthReportService healthReportService;
+        public HealthReportController(IHealthReportService healthReportService)
+        { 
+            this.healthReportService = healthReportService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<HealthReport>> GenerateHealthReport(string deviceId, int range)
+        {
+            var report = await healthReportService.GenerateHealthReportAsync(deviceId, range);
+            
+            return Ok(report);
+        }
+    }
+}
