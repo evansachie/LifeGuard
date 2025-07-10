e843abdc2bb4aed99d6edd4811ef20a7fece77d2
Evans Acheampong <evansachie01@gmail.com>
2025-07-10 11:38:10 +0000

    Revert "Add untracked files"
    This reverts commit df4316ed4da8051983f7ce2c845822ae2afac7c7.

--- web/src/data/timeframes.js
+++
@@ -1,7 +0,0 @@
-export const timeframes = [
-    { id: 'today', label: 'Today' },
-    { id: 'week', label: 'This Week' },
-    { id: 'month', label: 'This Month' },
-    { id: 'quarter', label: 'This Quarter' },
-    { id: 'year', label: 'This Year' }
-  ];
