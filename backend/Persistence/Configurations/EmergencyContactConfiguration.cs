64d8c9703ba1a2ca3d86913677197cab963f4f96
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-08-06 23:02:13 +0000

    feat(backend) : Add EmergencyContactConfiguration

---
+++ backend/Persistence/Configurations/EmergencyContactConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class EmergencyContactConfiguration : IEntityTypeConfiguration<EmergencyContacts>
+    {
+        public void Configure(EntityTypeBuilder<EmergencyContacts> builder)
+        {
+            builder.HasData(
+                new EmergencyContacts { 
+                
+                    Id = 1,
+                    Name = "Ambulance Service",
+                    Email = "info@moh.gov.gh",
+                    Phone = "0505982870",
+                    UserId = Guid.NewGuid().ToString(),
+
+                });
+        }
+
+    }
+}
