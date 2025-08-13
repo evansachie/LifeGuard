990fde0d1a9d5d0b1fdbf3b23260678ab277a60f
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-08-13 00:25:35 +0000

    feat(backend) : Add Status.cs

---
+++ backend/Domain/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Status
+    {
+        public bool Connected { get; set; }
+        public string DeviceName { get; set; }
+        public string LastDataKey { get; set; }
+        public long LastSeen { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
