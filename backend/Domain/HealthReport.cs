29348ef259c915b018ab912d8195d4692cd6e679
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-09-13 14:26:10 +0000

    Add air quality metrics to HealthReport a

--- backend/Domain/HealthReport.cs
+++ backend/Domain/HealthReport.cs
@@ -17,6 +17,14 @@ namespace Domain
         public double AvgAmbientTemp { get; set; }
         public double AvgHumidity { get; set; }
         public int AvgAirQualityIndex { get; set; }
+
+        public int Avgco2 { get; set; }
+        public int Avgvoc { get; set; }
+        public int Avgpm25 { get; set; }
+        public int Avgpm10 { get; set; }
+        public int Minaqi { get; set; }
+        public int Maxaqi { get; set; }
+        public int FallCount { get; set; } 
         public double AvgDailySteps { get; set; }
         public double AvgPressure { get; set; } 
         public DateTime LastUpdate { get; set; }
