c43dacf2146757a2bbd6e9c8b41c1aba0a76f8ca
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-09-17 15:13:46 +0000

    feat(backend) : Add AddReportHandler for handling report requests

---
+++ backend/Application/Features/Reports/Handlers/AddReportHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Contracts.Persistence;
+using Application.Features.Reports.Requests;
+using Application.Models.ApiResult;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Reports.Handlers
+{
+    public class AddReportHandler : IRequestHandler<AddReportRequest, Result>
+    {
+        private readonly IHealthReportRepository _healthReportRepository;
+
+        public AddReportHandler(IHealthReportRepository healthReportRepository)
+        {
+            _healthReportRepository = healthReportRepository;
+        }
+
+
+        public async Task<Result> Handle(AddReportRequest request, CancellationToken token)
+        {
+            var result = await _healthReportRepository.Add(request.HealthReport);
+
+            return new Result(true, ResultStatusCode.Success, "Report Added Successfully");
+
+        }
+
+
+
+
+    }
+}
