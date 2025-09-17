780a546b0b06ff824076c1454b9e5ecde51cd8ec
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-09-17 15:16:22 +0000

    feat(backend) : Add request classes for report management .Implement three new request classes: AddReportRequest, DeleteReportRequest, and GetReportRequest in the Application.Features.Reports.Requests namespace.

---
+++ backend/Application/Features/Reports/Requests/AddReportRequest.cs
@@ -0,0 +1,17 @@
+﻿using Application.Models.ApiResult;
+using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Reports.Requests
+{
+    public class AddReportRequest : IRequest<Result>
+    {
+        public HealthReport HealthReport;
+        
+    }
+}
