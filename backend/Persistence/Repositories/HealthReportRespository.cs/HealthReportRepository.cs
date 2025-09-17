85897e0d4a9324033c6b710bb1413d56e591217c
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-09-17 15:18:29 +0000

    feat(backend) : Add HealthReportRepository with user report retrieval

---
+++ backend/Persistence/Repositories/HealthReportRespository.cs/HealthReportRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories.HealthReportRespository
+{
+    public class HealthReportRepository : GenericRepository<HealthReport>, IHealthReportRepository
+    {
+        private readonly LifeGuardDbContext _dbContext;
+        public HealthReportRepository(LifeGuardDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IReadOnlyList<HealthReport>> GetReportByUserIdAsync(string userId)
+        {
+            var reports =   _dbContext.HealthReports.
+                Where(r => r.UserId == userId);
+
+            return new List<HealthReport>(reports);
+        }
+
+
+    }
+}
