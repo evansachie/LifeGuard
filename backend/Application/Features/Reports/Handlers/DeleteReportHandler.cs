7a2e938737fe9bf91d3932945238395f284eb4f6
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-09-17 15:14:19 +0000

    Add DeleteReportHandler for report deletion functionality

---
+++ backend/Application/Features/Reports/Handlers/DeleteReportHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Contracts.Persistence;
+using Application.Features.Reports.Requests;
+using Application.Models.ApiResult;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Reports.Handlers
+{
+    public class DeleteReportHandler : IRequestHandler<DeleteReportRequest, Result>
+    {
+        private readonly IHealthReportRepository _healthReportRepository;
+
+        public DeleteReportHandler(IHealthReportRepository healthReportRepository)
+        {
+            _healthReportRepository = healthReportRepository;
+        }
+
+
+        public async Task<Result> Handle(DeleteReportRequest request, CancellationToken token)
+        {
+            await _healthReportRepository.Delete(request.HealthReport);
+
+            return new Result(true, ResultStatusCode.Success, "Report Added Successfully");
+
+        }
+    }
+}
