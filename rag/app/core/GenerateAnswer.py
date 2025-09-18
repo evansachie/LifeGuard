634894dc18e9205564e59d90bd45a105fb097e39
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-09-18 08:43:45 +0000

    refactor(rag) : Remove print statement

--- rag/app/core/GenerateAnswer.py
+++ rag/app/core/GenerateAnswer.py
@@ -25,8 +25,7 @@ def generate_answer(state: MessagesState):
     """Generate an answer."""
     question = state["messages"][0].content
     context = state["messages"][-1].content
-
-    print("context\n", context)
+    
     prompt = GENERATE_PROMPT.format(question=question, context=context)
     response = response_model.invoke([{"role": "user", "content": prompt}])
     return {"messages": [response]}
