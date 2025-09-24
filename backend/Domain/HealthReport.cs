c3a189ca566b450e5a135666db8d7dd139547bbc
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-09-24 22:40:21 +0000

    feat(backend) : Update health report data types and add new properties

--- backend/Domain/HealthReport.cs
+++ backend/Domain/HealthReport.cs
@@ -16,15 +16,19 @@ namespace Domain
         public double AvgAmbientTemp { get; set; }
         public double AvgHumidity { get; set; }
         public int AvgAirQualityIndex { get; set; }
-
-        public int Avgco2 { get; set; }
-        public int Avgvoc { get; set; }
-        public int Avgpm25 { get; set; }
-        public int Avgpm10 { get; set; }
+        public double Avgco2 { get; set; }
+        public double Avgvoc { get; set; }
+        public double Avgpm25 { get; set; }
+        public double Avgpm10 { get; set; }
         public int Minaqi { get; set; }
         public int Maxaqi { get; set; }
+        public double AvgbloodPressureSystolic { get; set; }
+        public double AvgbloodPressureDiastolic { get; set; }
+        public double AvgBodyTemperature { get; set; }
+        public double AvgHeartRate { get; set; }
+        public double AvgOxygenSaturation { get; set; }   
         public int FallCount { get; set; } 
-        public double AvgDailySteps { get; set; }
+        public int AvgDailySteps { get; set; }
         public double AvgPressure { get; set; } 
         public DateTime LastUpdate { get; set; }
         public string Status { get; set; }
