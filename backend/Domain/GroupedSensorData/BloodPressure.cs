6e323e23bf20868d073f7ac98a1a7ab61b94e540
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-09-24 22:37:07 +0000

    feat(backend) : Add BloodPressure class to GroupedSensorData namespace

---
+++ backend/Domain/GroupedSensorData/BloodPressure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.GroupedSensorData
+{
+    public class BloodPressure
+    {
+        public double diastolic;
+
+        public double systolic;
+
+    }
+}
